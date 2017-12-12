4e8d33aa82904f628f39288e2bd2a0c618d23df3
Chris Evans <evanschris@users.noreply.github.com>
2017-12-12 10:32:31 +0000

    test 0 instead of false
    false dosent seem to be working

--- app/assets/javascripts/geff/alerts.js
+++ app/assets/javascripts/geff/alerts.js
@@ -3,7 +3,7 @@ $.fn.alerts = function(timeOut) {
 
   timeOut = timeOut || 5000;
 
-  if (timeOut != false) {
+  if (timeOut != 0) {
     setTimeout(function() {
       $('#alerts').slideUp();
     }, timeOut);
