9c5f154d51214c3cbd07b2f98943f33c40443dd9
Chris Evans <evanschris@users.noreply.github.com>
2017-12-12 10:34:22 +0000

    declare default variabel inside if statement
    default is possibly getting overided because it is 0/false - this may or may not work

--- app/assets/javascripts/geff/alerts.js
+++ app/assets/javascripts/geff/alerts.js
@@ -1,9 +1,8 @@
 $.fn.alerts = function(timeOut) {
   if (this.length === 0) { return false; }
 
-  timeOut = timeOut || 5000;
-
   if (timeOut != 0) {
+    timeOut = timeOut || 5000;
     setTimeout(function() {
       $('#alerts').slideUp();
     }, timeOut);
