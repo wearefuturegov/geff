e072f252325f54ae7e0465bf17db78cd586bdc2c
Chris Evans <evanschris@users.noreply.github.com>
2018-03-12 16:20:17 +0000

    add some notes for future iterations of the accordion functionality

--- app/assets/javascripts/geff/accordion.js
+++ app/assets/javascripts/geff/accordion.js
@@ -1,5 +1,14 @@
 $.fn.accordion = function() {
-  if (this.length === 0) { return false; }
+  if (this.length === 0) { return false; }\
+
+  /*
+    A few things that would be nice to add to this
+    - there could be a pre built in fa-chevron-down or some form of icon to indicate it can be opened, which when clicked could be animated with CSS to rotate to the upwards direction.
+    - Customised features:
+      - May want to cusomtise the speed of the slide toggle (if slower then what happens if one is clicked while the other is animating?)
+      - May want the ability to choose between multiple independant accodrions or all accordions interacting on a page for sites with more than one.
+    - anything else?
+  */
 
   var parentAccord = $(this)
 
